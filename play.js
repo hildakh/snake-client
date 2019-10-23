0f5cb481af65fe39c164eb7bf08758687af6fac6
hildakh <h_khajehamiri@yahoo.com>
2019-10-23 21:31:14 +0000

    Moved the functions to the client side

--- play.js
+++ play.js
@@ -1,18 +1,7 @@
-const net = require('net');
-
+const connect = require('./client');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '192.168.88.149',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
 
 console.log('Connecting ...');
 connect();
