0e0087035638dc76340389f805326a04fd5e8166
hildakh <h_khajehamiri@yahoo.com>
2019-10-23 22:50:55 +0000

    refactored

--- play.js
+++ play.js
@@ -1,7 +1,44 @@
-const connect = require('./client');
-/**
- * Establishes connection with the game server
- */
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
+
+setupInput();
+/**
+ * Establishes connection with the game server
+ */
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding('utf8');
+//   stdin.resume();
+//   const handleUserInput = function (cb) {
+//     stdin.on('data', data => {
+//       cb(data)
+//     })
+//   }
+//   return handleUserInput;
+// }
+
+console.log('Connecting ...');
+let conn = connect();
+// const handler = setupInput()
+
+// handler(input => {
+//       if (input === "w") {
+//         console.log("up")
+//         conn.write("Move: up");
+//       }
+//       if (input === 's') {
+//         conn.write("Move: down");
+//       }
+//       if (input === 'a') {
+//         conn.write("Move: left");
+//       }
+//       if (input === 'd') {
+//       conn.write("Move: right");
+//       }
+//       if(input === '\u0003')
+//       process.exit();
+//     })
