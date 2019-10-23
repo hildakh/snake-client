f4872d8fde58a86d7ef00451f31e31c3a4560a20
hildakh <h_khajehamiri@yahoo.com>
2019-10-23 22:49:37 +0000

    refactored

--- client.js
+++ client.js
@@ -1,9 +1,15 @@
 const net = require('net');
-const stdin = process.stdin
 
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding('utf8');
+//   stdin.resume();
+//   return stdin;
 
+// }
 
-const connect = function () {
+const connect = function (stdin) {
   const conn = net.createConnection({
     host: '192.168.88.149',
     port: 50541
@@ -11,41 +17,21 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
-
-
   conn.on('connect', connect => {
     console.log('Successfully connected to game server');
     conn.write("Name: HLD");
-    
-    stdin.setRawMode(true);
-    stdin.setEncoding('utf8');
-    stdin.resume();
-  
-  
 
-    stdin.on('data', data => {
-      console.log("Data: ",data)
-      if (data === "w") {
-        console.log("up")
-        conn.write("Move: up");
-      }
-      if (data === 's') {
-        conn.write("Move: down");
-      }
-      if (data === 'a') {
-        conn.write("Move: left");
-      }
-      if (data === 'd') {
-      conn.write("Move: right");
-      }
-      if(data === '\u0003')
-      process.exit();
-    })
+});
+  //     if(data === '\u0003')
+  //     process.exit();
+  //   })
 
-  })
+  // })
 
   return conn;
-}
+};
+
 
 module.exports = connect;
+// module.exports = setupInput;
 
